e83adf918939a1de1df8acd95e6e89e0f9b6b5b6
higorlaf12 <higor.freitas@neppoadm.com.br>
2017-10-15 23:37:22 -0200

    New project SeleniumTrenamento

---
+++ SeleniumAprendendo/src/main/java/com/SeleniumAprendendo/SeleniumAprendendo/SeleniumAprendendoApplication.java
@@ -0,0 +1,12 @@
+package com.SeleniumAprendendo.SeleniumAprendendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumAprendendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumAprendendoApplication.class, args);
+	}
+}
